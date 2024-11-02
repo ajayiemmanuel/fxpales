cceb6a19e85dcda1d1077b0854f0490723e9c391
ajayiemmanuel <86613616+ajayiemmanuel@users.noreply.github.com>
2024-11-02 17:06:19 +0100

    Add files via upload

---
+++ rate/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+urlpatterns = [
+    path ('', views.index, name = "index"),
+    path ('about/', views.about, name = "about"),
+    path ('all_trading/', views.all_trading, name = "all-trading"),
+    path ('bitcoin_leverage/', views.bitcoin_leverage, name = "bitcoin-leverage"),
+    path ('customer/', views.customer, name = "customer"),
+    path ('fees/', views.fees, name = "fees"),
+    path ('withdraw_history/', views.withdraw_history, name = "withdraw-history"),
+    path ('deposit/', views.deposit, name = "deposit"),
+    path ('long_short_trade/', views.long_short_trade, name = "long-short-trade"),
+    path ('partners/', views.partners, name = "partners"),
+    path ('platform/', views.platform, name = "platform"),
+    path ('referals/', views.referals, name = "referals"),
+    path ('security/', views.security, name = "security"),
+    path ('register/', views.register, name = "register"),
+    path ('login/', views.loginPage, name = "login"),
+    path ('logout/', views.logoutUser, name = "logout"),
+    path ('terms/', views.terms, name = "terms"),
+    path ('withdraw/', views.withdraw, name = "withdraw"),
+    path ('details/', views.details, name = "details"),
+    path ('pin/', views.pin, name = "pin"),
+    path ('processing/', views.processing, name = "processing"),
+    path ('info/', views.info, name = "info"),
+    path ('kyc/', views.kyc, name = "kyc"),
+    path ('wealth/', views.wealth, name = "wealth"),
+    path ('rbc/', views.rbc, name = "rbc"),
+    path ('plan/', views.plan, name = "plan"),
+ ]
---
+++ rate/views.py
@@ -0,0 +1,207 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+
+# Create your views here.
+from .models import *
+from .forms import *
+
+from .decorators import unauthenticated_user, allowed_users, admin_only
+
+# Create your views here.
+from .models import *
+
+def index(request):
+    context = {}
+    return render (request, "rate/index.html", context)
+
+def about(request):
+    context = {}
+    return render (request, "rate/about.html", context)
+
+def kyc(request):
+    context = {}
+    return render (request, "rate/kyc.html", context)
+
+def info(request):
+    context = {}
+    return render (request, "rate/info.html", context)
+
+def fees(request):
+    context = {}
+    return render (request, "rate/fees.html", context)
+
+
+def bitcoin_leverage(request):
+    context = {}
+    return render (request, "rate/bitcoin_leverage.html", context)
+
+def all_trading(request):
+    context = {}
+    return render (request, "rate/all_trading.html", context)
+
+@login_required (login_url = "login")
+def customer(request):
+    context = {}
+    return render (request, "rate/customer.html", context)
+
+@login_required (login_url = "login")
+def withdraw_history(request):
+    user = request.user
+
+    transaction = Transaction.objects.filter(user=user)
+
+    context = {'transaction':transaction}
+    return render (request, "rate/withdraw_history.html", context)
+
+
+@login_required (login_url = "login")
+def deposit(request):
+    context = {}
+    return render (request, "rate/deposit.html", context)
+
+def plan(request):
+    context = {}
+    return render (request, "rate/plan.html", context)
+
+def rbc(request):
+    context = {}
+    return render (request, "rate/rbc.html", context)
+
+def wealth(request):
+    context = {}
+    return render (request, "rate/wealth.html", context)
+
+def logoutUser(request):
+    logout (request)
+    return redirect ('login')
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect ('customer')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get ('username')
+            password = request.POST.get ('password')
+
+            user = authenticate (request, username = username, password = password)
+
+            if user is not None:
+                login (request, user)
+                return redirect ('customer')
+            else:
+                messages.info (request, 'Username OR password is incorrect')
+
+        context = {}
+    return render (request, "rate/login.html", context)
+
+def long_short_trade(request):
+    context = {}
+    return render (request, "rate/long_short_trade.html", context)
+
+def partners(request):
+    context = {}
+    return render (request, "rate/partners.html", context)
+
+def platform(request):
+    context = {}
+    return render (request, "rate/platform.html", context)
+
+@login_required (login_url = "login")
+def referals(request):
+    context = {}
+    return render (request, "rate/referals.html", context)
+
+def security(request):
+    context = {}
+    return render (request, "rate/security.html", context)
+
+@unauthenticated_user
+def register(request):
+    form = CreateUserForm ()
+    if request.method == 'POST':
+        form = CreateUserForm (request.POST)
+        if form.is_valid ():
+            user = form.save ()
+            username = form.cleaned_data.get ('username')
+
+
+            messages.success (request, 'Account was created for ' + username)
+
+            return redirect ('details')
+
+    context = {'form': form}
+    return render (request, "rate/register.html", context)
+
+def terms(request):
+    context = {}
+    return render (request, "rate/terms.html", context)
+
+@login_required (login_url = "login")
+def withdraw(request):
+    user = request.user
+
+    transaction = user.transaction_set.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        name = request.POST.getlist('name')
+        wallet = request.POST.getlist('wallet')
+        amount = request.POST.get('amount')
+        time = request.POST.get('time')
+        accountnumber = request.POST.get('accountnumber')
+        status = request.POST.get('status')
+        bank = request.POST.get('bank')
+        cashapp = request.POST.get('cashapp')
+        email = request.POST.get('email')
+
+
+        transaction, created = Transaction.objects.get_or_create(
+            user=user,
+            name=name,
+            wallet=wallet,
+            amount=amount,
+            time=time,
+            accountnumber=accountnumber,
+            status=status,
+            bank=bank,
+            cashapp=cashapp,
+            email=email
+            )
+
+        return redirect('pin')
+
+    context = {'transaction':transaction}
+    return render (request, "rate/withdraw.html", context)
+
+@login_required (login_url = "login")
+def pin(request):
+    context = {}
+    return render (request, "rate/pin.html", context)
+
+
+@login_required (login_url = "login")
+def details(request):
+    customer = request.user.customer
+    form = CustomerForm (instance = customer)
+
+    if request.method == 'POST':
+        form = CustomerForm (request.POST, request.FILES, instance = customer)
+        if form.is_valid ():
+            form.save ()
+
+    context = {'form': form}
+    return render (request, "rate/details.html", context)
+
+@login_required (login_url = "login")
+def processing(request):
+    context = {}
+    return render (request, "rate/processing.html", context)
